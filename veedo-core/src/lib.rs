3c43b6550e24919938893c3a317e238365fda256
Jonathan LEI <me@xjonathan.dev>
2024-09-29 14:54:22 +0800

    perf: use two root value slots for thread sync

--- veedo-core/src/lib.rs
+++ veedo-core/src/lib.rs
@@ -1,15 +1,32 @@
-use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
+use std::sync::atomic::{AtomicU64, AtomicUsize, Ordering};
 
 use veedo_ff::{BigInteger128, Field, FieldElement};
 
 mod constants;
 use constants::{MDS_MATRIX, ROUND_CONSTANTS};
 
+/// The computation thread state that's shared between the 2 worker threads.
+///
+/// Two slots are used as it's possible for one thread to have the next cube root ready before the
+/// other thread has even consumed the current root. With just one slot, the current thread would
+/// then have to check whether the existing value has been consumed by the other thread or not,
+/// adding overhead to the synchronization.
+///
+/// Having more than 2 slots would not add any performance benefit, as it's impossible for one
+/// thread to get ahead of the other by more than one slot due to the matrix multiplication step.
 #[derive(Debug, Clone)]
 struct SyncState<'a> {
-    low: &'a AtomicU64,
-    high: &'a AtomicU64,
-    ready: &'a AtomicBool,
+    even_root: &'a AtomicFieldElement,
+    odd_root: &'a AtomicFieldElement,
+    count: &'a AtomicUsize,
+}
+
+/// A [`FieldElement`] value backed by two [`AtomicU64`] fields to be used in the thread
+/// synchronization context.
+#[derive(Debug, Default)]
+struct AtomicFieldElement {
+    low: AtomicU64,
+    high: AtomicU64,
 }
 
 pub fn compute_delay_function(n_iters: usize, x: u128, y: u128) -> (FieldElement, FieldElement) {
@@ -17,14 +34,14 @@ pub fn compute_delay_function(n_iters: usize, x: u128, y: u128) -> (FieldElement
     let y_montgomery = FieldElement::from(y);
 
     let x_state = SyncState {
-        low: &AtomicU64::new(0),
-        high: &AtomicU64::new(0),
-        ready: &AtomicBool::new(false),
+        even_root: &AtomicFieldElement::default(),
+        odd_root: &AtomicFieldElement::default(),
+        count: &AtomicUsize::new(0),
     };
     let y_state = SyncState {
-        low: &AtomicU64::new(0),
-        high: &AtomicU64::new(0),
-        ready: &AtomicBool::new(false),
+        even_root: &AtomicFieldElement::default(),
+        odd_root: &AtomicFieldElement::default(),
+        count: &AtomicUsize::new(0),
     };
 
     std::thread::scope(|s| {
@@ -50,19 +67,25 @@ fn compute_in_thread<const SLOT: usize>(
 ) -> FieldElement {
     let mut current = init_value;
 
+    let mut count = 0;
+
     for round_constant in ROUND_CONSTANTS.iter().cycle().take(n_iters) {
         // Compute cube root. This is the most expensive part of the iteration.
         current = cube_root(&current);
 
-        // Make the current thread's state available to the other thread.
-        loop {
-            if !current_state.ready.load(Ordering::Acquire) {
-                break;
-            }
-        }
-        current_state.low.store(current.0 .0[0], Ordering::Release);
-        current_state.high.store(current.0 .0[1], Ordering::Release);
-        current_state.ready.store(true, Ordering::Release);
+        count += 1;
+        let is_even = count % 2 == 0;
+
+        // There's no need to make sure the other thread has already read the slot, as it's simply
+        // impossible for the current thread to be 2 slots ahead of the other.
+        let current_root = if is_even {
+            current_state.even_root
+        } else {
+            current_state.odd_root
+        };
+        current_root.low.store(current.0 .0[0], Ordering::Release);
+        current_root.high.store(current.0 .0[1], Ordering::Release);
+        current_state.count.store(count, Ordering::Release);
 
         // Finish half of the matrix multiplication and constant addition steps which do not depend
         // on the other thread.
@@ -71,16 +94,21 @@ fn compute_in_thread<const SLOT: usize>(
         // Wait for the state value from the other thread
         let other = {
             loop {
-                if other_state.ready.load(Ordering::Acquire) {
+                if other_state.count.load(Ordering::Acquire) >= count {
                     break;
                 }
             }
 
-            let other_low = other_state.low.load(Ordering::Acquire);
-            let other_high = other_state.high.load(Ordering::Acquire);
-            other_state.ready.store(false, Ordering::Release);
+            let other_root = if is_even {
+                other_state.even_root
+            } else {
+                other_state.odd_root
+            };
 
-            FieldElement::new_unchecked(BigInteger128::new([other_low, other_high]))
+            FieldElement::new_unchecked(BigInteger128::new([
+                other_root.low.load(Ordering::Acquire),
+                other_root.high.load(Ordering::Acquire),
+            ]))
         };
 
         // Finish the round by completing the other half of the matrix multiplication.
